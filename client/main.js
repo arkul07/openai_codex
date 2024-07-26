71b9a046180c84c0cc115b149dc19ca8b56dd37e
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2024-07-25 19:22:13 -0700

    updated site

--- client/main.js
+++ client/main.js
@@ -74,7 +74,7 @@ const handleSubmit = async e => {
 
   //fetch data from server == bot's response
 
-  const response = await fetch('http://localhost:4000', {
+  const response = await fetch('https://openai-codex-fz6h.onrender.com', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
